71b59861dff7e2d983cf21999b74f0d7013bd640
ABSphreak <absphreak@outlook.com>
2020-02-24 12:45:37 +0530

    ✏ Editing HomePage

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,90 +1,26 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.layout')
 
-		<title>Laravel</title>
+@section('content')
+<div class="flex-center position-ref full-height">
 
-		<!-- Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-		<!-- Styles -->
-		<style>
-				html, body {
-						background-color: #fff;
-						color: #636b6f;
-						font-family: 'Nunito', sans-serif;
-						font-weight: 200;
-						height: 100vh;
-						margin: 0;
-				}
-
-				.full-height {
-						height: 100vh;
-				}
-
-				.flex-center {
-						align-items: center;
-						display: flex;
-						justify-content: center;
-				}
-
-				.position-ref {
-						position: relative;
-				}
-
-				.top-right {
-						position: absolute;
-						right: 10px;
-						top: 18px;
-				}
-
-				.content {
-						text-align: center;
-				}
-
-				.title {
-						font-size: 84px;
-				}
-
-				.links > a {
-						color: #636b6f;
-						padding: 0 25px;
-						font-size: 13px;
-						font-weight: 600;
-						letter-spacing: .1rem;
-						text-decoration: none;
-						text-transform: uppercase;
-				}
-
-				.m-b-md {
-						margin-bottom: 30px;
-				}
-		</style>
-</head>
-<body>
-		<div class="flex-center position-ref full-height">
-
-				<div class="content">
-						<div class="title m-b-md">
-								Pizza List
-						</div>
-
-
-						@foreach ($pizzas as $pizza)
-							<div>
-								{{$loop->index}} {{$pizza['type']}} - {{$pizza['base']}} → {{$pizza['price']}}
-								@if($loop->first)
-									<span>- first in the loop</span>
-								@endif
-								@if($loop->last)
-									<span>- last in the loop</span>
-								@endif
-							</div>
-						@endforeach
-
+		<div class="content">
+				<div class="title m-b-md">
+						Pizza List
 				</div>
+
+
+				@foreach ($pizzas as $pizza)
+					<div>
+						{{$loop->index}} {{$pizza['type']}} - {{$pizza['base']}} → {{$pizza['price']}}
+						@if($loop->first)
+							<span>- first in the loop</span>
+						@endif
+						@if($loop->last)
+							<span>- last in the loop</span>
+						@endif
+					</div>
+				@endforeach
+
 		</div>
-	</body>
-</html>
+</div>
+@endsection

603b48ea31109ffa43f4cf58de0db69fbce82d6a
ABSphreak <absphreak@outlook.com>
2020-03-02 17:18:49 +0530

    🔐 Setting up Auth

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/pizzas/create', 'PizzaController@create');
 Route::post('/pizzas', 'PizzaController@store');
 Route::get('/pizzas/{id}', 'PizzaController@show');
 Route::delete('/pizzas/{id}', 'PizzaController@destroy');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
